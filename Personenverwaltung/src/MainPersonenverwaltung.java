0b3b70733eae3746618c07c9001e21248f33c424
Severin <severin.gafner@gmail.com>
2016-09-23 15:01:17 +0200

    Revert "Revert "Struktur des Projektes""
    This reverts commit 4c4ca004ee9dad32d8b8f03ce478e40c28b34db4.

---
+++ Personenverwaltung/src/MainPersonenverwaltung.java
@@ -0,0 +1,17 @@
+/**
+ * Dieses Interface beinhaltet die Defaultabstände für das TableLayout.
+ * 
+ * 16.09.2016 gafner	File erstellt.
+ * 
+ * 
+ * @author Severin Gafner
+ *
+ */
+public class MainPersonenverwaltung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Personenverwaltung/src/view/FieldPanel.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.JPanel;
+/**
+ * Dieses Interface beinhaltet die Defaultabstände für das TableLayout.
+ * 
+ * 16.09.2016 gafner	File erstellt.
+ * 
+ * 
+ * @author Severin Gafner
+ *
+ */
+public class FieldPanel extends JPanel {
+
+	public FieldPanel() {
+		double size[][] = {
+				{},
+				{}
+		};
+	}
+
+}
